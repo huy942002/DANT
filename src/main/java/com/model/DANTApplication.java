693a4d05e04576d05a1766b99bd18430664903b1
huy942002 <huy942002@gmail.com>
2024-05-24 20:20:17 +0700

    updateNameProject

---
+++ src/main/java/com/model/DANTApplication.java
@@ -0,0 +1,12 @@
+package com.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DANTApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DANTApplication.class, args);
+	}
+}
